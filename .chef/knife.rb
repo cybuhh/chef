994341d8b1b73ecf9f2b2578d6f17a0640f55608
cybuhh <mateusz.cyburt@gmail.com>
2013-12-16 20:45:04 +0100

    ignroe update & moved knife config

---
+++ .chef/knife.rb
@@ -0,0 +1,6 @@
+current_dir = File.dirname(__FILE__)
+cookbook_path ["#{current_dir}/cookbooks"]
+
+cookbook_copyright "Foo"
+cookbook_email     "mateusz.cyburt@gmail.com"
+cookbook_license   "proprietary"
