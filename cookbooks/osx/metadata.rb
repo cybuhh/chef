acd62e107904f05b37ff64c42d2df5df7862b734
cybuhh <mateusz.cyburt@gmail.com>
2013-12-24 16:14:27 +0100

    added recipe textmate-bundles and sample doc files

---
+++ cookbooks/osx/metadata.rb
@@ -0,0 +1,7 @@
+name             'osx'
+maintainer       'cybuhh'
+maintainer_email 'mateusz.cyburt@gmail.com'
+license          ''
+description      'Installs/Configures osx'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
