8e0669c948b2bb886eefc200176c14f2aa9c3e7e
cybuhh <mateusz.cyburt@gmail.com>
2014-02-01 18:30:46 +0100

    removed xfce4 panel config, added chromium kiosk mode autostart item

--- cookbooks/qs-stp/recipes/x.rb
+++ cookbooks/qs-stp/recipes/x.rb
@@ -22,15 +22,9 @@ end
 
 [
     ".config",
+    ".config/autostart",
     ".config/xfce4",
     ".config/xfce4/desktop",
-    ".config/xfce4/xfconf",
-    ".config/xfce4/xfconf/xfce-perchannel-xml",
-    ".config/xfce4/panel",
-    ".config/xfce4/panel/launcher-9",
-    ".config/xfce4/panel/launcher-10",
-    ".config/xfce4/panel/launcher-11",
-    ".config/xfce4/panel/launcher-12",
 ].each do |tplPath|
     directory "/home/#{node[:defaultUser]}/#{tplPath}" do
         owner node[:defaultUser]
@@ -41,31 +35,14 @@ end
     end
 end
 
-{
-    "desktop" => "icons.screen0.rc",
-    "xfconf/xfce-perchannel-xml" => "xfce4-mixer.xml",
-    "xfconf/xfce-perchannel-xml" => "xfce4-panel.xml",
-    "xfconf/xfce-perchannel-xml" => "xsettings.xml",
-    "xfconf/xfce-perchannel-xml" => "xfwm4.xml",
-    "panel/launcher-9" => "13912645871.desktop",
-    "panel/launcher-10" => "13912645882.desktop",
-    "panel/launcher-11" => "13912645883.desktop",
-    "panel/launcher-12" => "13912645884.desktop"
-}.each_pair do |tplPath, tplFile| 
+template "/home/#{node[:defaultUser]}/.config/autostart/chromium-autostart.desktop" do
+	source "chromium-autostart.desktop"
+    owner node[:defaultUser]
+    group node[:defaultUser]
+    mode 0644
+    :create
+end
 
-#	directory "/home/#{node[:defaultUser]}/.config/xfce4/#{tplPath}" do
-#	    owner node[:defaultUser]
-#	    group node[:defaultUser]
-#	    mode 0755
-#	    recursive true
-#	    action :create
-#	end
-
-	template "/home/#{node[:defaultUser]}/.config/xfce4/#{tplPath}/#{tplFile}" do
-    	source "xfce4/#{tplPath}/#{tplFile}"
-	    owner node[:defaultUser]
-	    group node[:defaultUser]
-	    mode 0644
-        :create
-    end
+service "lightdm" do
+    action :restart
 end
