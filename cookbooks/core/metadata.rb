4255e7f23ccc709d38b3bfbff39a6d87bb107925
cybuhh <mateusz.cyburt@gmail.com>
2014-09-29 16:11:35 +0200

    metadata update

--- cookbooks/core/metadata.rb
+++ cookbooks/core/metadata.rb
@@ -1,7 +1,7 @@
-name             'qs-stp'
+name             'core'
 maintainer       'cybuhh'
 maintainer_email ''
 license          ''
-description      'Installs/Configures quality station for STP'
+description      'Installs/Configures core utils'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
--- cookbooks/osx/metadata.rb
+++ cookbooks/osx/metadata.rb
@@ -1,6 +1,6 @@
 name             'osx'
 maintainer       'cybuhh'
-maintainer_email 'mateusz.cyburt@gmail.com'
+maintainer_email ''
 license          ''
 description      'Installs/Configures osx'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
