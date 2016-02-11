7e08b8fad2836bb1f93355fcecd33aab402c9c41
cybuhh <mateusz.cyburt@gmail.com>
2016-02-11 19:11:00 +0100

    x11 vnc fixed and rpi node as well

--- cookbooks/x11/metadata.rb
+++ cookbooks/x11/metadata.rb
@@ -1,7 +1,7 @@
-name             ''
+name             'x11'
 maintainer       'cybuhh'
 maintainer_email ''
 license          ''
-description      'Installs/Configures quality station for STP'
+description      'Installs/Configures X11 components'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
