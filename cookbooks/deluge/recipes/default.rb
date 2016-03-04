d186288c29a9364c98abc427b1bd1a60b7fee67b
cybuhh <mateusz.cyburt@gmail.com>
2016-03-04 13:12:20 +0100

    updated cookbook::deluge and added role::rpi

--- cookbooks/deluge/recipes/default.rb
+++ cookbooks/deluge/recipes/default.rb
@@ -13,3 +13,19 @@
     action :install
   end
 end
+
+cookbook_file "/home/#{node[:default_user]}/.config/deluge/core.conf" do
+    source "core.conf"
+    mode 0644
+    owner node[:default_user]
+    group node[:default_user]
+    action :create
+    backup 1
+    notifies :run, "execute[replace_placeholders]"
+end
+
+execute "replace_placeholders" do
+    file_path = "/home/#{node[:default_user]}/.config/deluge/core.conf"
+    command "sed -i -e 's/__USER__/#{node[:default_user]}/g' #{file_path}"
+    action :nothing
+end
